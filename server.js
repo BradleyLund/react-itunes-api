f15e07c46fde548fa5914e7eb54a01ad3c33e753
Bradley Lund <bradjlund@gmail.com>
2021-08-21 19:48:52 +0200

    get *

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ require("isomorphic-fetch");
 
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("/", function (req, res) {
+app.get("/*", function (req, res) {
   res.sendFile(path.resolve(__dirname, "build", "index.html"));
 });
 
